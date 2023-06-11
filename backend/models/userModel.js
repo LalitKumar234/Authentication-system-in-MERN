b7ad7852e3aa30b6510f260aaa908e098159bc82
lalit sahu <lsahu0838@gmail.com>
2023-06-11 22:45:31 +0530

    Backend fixes

---
+++ backend/models/userModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const UserSchema = mongoose.Schema({
+    firstname:{
+        type: String,
+        required: true,
+    },
+    lastname:{
+        type: String,
+        required: true,
+    },
+    email:{
+        type: String,
+        required: true,
+    },
+    password:{
+        type: String,
+        required: true,
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now()
+    }
+})
+
+module.exports = mongoose.model("user", UserSchema);
+
+
